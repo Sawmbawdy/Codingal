bda45778cf2e3f2d970369338d0254c340c22182
Sawmbawdy <saiompatro12@gmail.com>
2025-07-16 17:09:53 -0400

    Add files via upload

---
+++ CheckingGreaterOrSmaller.py
@@ -0,0 +1,10 @@
+i = int(input("Give an integer for i: "))
+if i > 15:
+    print("i is greater than 15")
+    print("I'm using an if command")
+elif i < 15:
+    print("i is less than 15")
+    print("I'm using and elif command")
+else:
+    print("i is equal to 15")
+    print("This is an else command")
