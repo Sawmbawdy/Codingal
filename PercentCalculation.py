1f3ff679916247bef2ac777b028f937a812514e0
Sawmbawdy <saiompatro12@gmail.com>
2025-07-16 17:00:21 -0400

    Add files via upload

---
+++ PercentCalculation.py
@@ -0,0 +1,16 @@
+print("Put your marks obtained in the 4 subjects")
+Math = int(input("Enter you Math Score Here: "))
+
+English = int(input("Enter your English Score Here: "))
+
+Hindi = int(input("Enter your Hindi Score Here: "))
+
+Science = int(input("Enter your Science Score Here: "))
+
+Sum = Math+English+Hindi+Science
+
+percent = (Sum/400)*100
+
+print("Your Average Percent Is \n", percent)
+
+print("Imagine getting a % \n", percent)
